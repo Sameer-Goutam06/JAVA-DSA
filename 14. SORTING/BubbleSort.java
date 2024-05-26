076368f9412eae6de2980d45fb427d7cde83479f
Sameer-Goutam06 <r.n.v.s.sgautham@gmail.com>
2024-05-26 19:59:15 +0530

    Sorting Intro

---
+++ 14. SORTING/BubbleSort.java
@@ -0,0 +1,29 @@
+//O(n^2) time complexity
+//checks each and every element and swaps it into ascending or to descending
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args)
+    {
+        int[] arr={5,4,2,1,3};
+        System.out.println(Arrays.toString(bubbleSort(arr)));
+    }
+    public static int[] bubbleSort(int[] arr)
+    {
+        int n=arr.length;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(arr[i]<arr[j])
+                {
+                    int temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        return arr;
+    }
+}
