7ba470cbc8b94f1039760e6ad84d2a80325b4b49
Sameer-Goutam06 <r.n.v.s.sgautham@gmail.com>
2024-05-27 12:39:24 +0530

    Spiral matrix solved

---
+++ 15. 2D Arrays/SpiralMatrix.java
@@ -0,0 +1,34 @@
+public class SpiralMatrix {
+    public static void main(String[] args) {
+        int[][] matrix={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+        //traverse through the matrix in spiral way
+        int startRow=0,endRow=matrix.length -1,startCol=0,endCol=matrix[0].length -1;
+        while(startRow<=endRow && startCol<=endCol)
+        {
+            //top traversal
+            for(int i=startCol;i<=endCol;i++)
+            {
+                System.out.print(matrix[startRow][i]+" ");
+            }
+            //right to bottom traversal
+            for(int i=startRow+1;i<=endRow;i++)
+            {
+                System.out.print(matrix[i][endCol]+" ");
+            }
+            //botttom to left traversal
+            for(int i=endCol-1;i>=startCol;i--)
+            {
+                System.out.print(matrix[endRow][i]+" ");
+            }
+            //left to top-1 traversal
+            for(int i=endRow-1;i>=startRow+1;i--)
+            {
+                System.out.print(matrix[i][startCol]+" ");
+            }
+            startRow++;
+            startCol++;
+            endCol--;
+            endRow--;
+        }
+    }
+}
